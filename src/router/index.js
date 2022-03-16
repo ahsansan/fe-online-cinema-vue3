222e5a908285afda8b75571e871c219700212109
Ahsan <achsan.ajr@gmail.com>
2022-03-16 12:26:35 +0700

    router for user

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import store from "../store/index";
 
 const routes = [
   {
@@ -21,22 +22,46 @@ const routes = [
     path: "/profile",
     name: "profile",
     component: () => import("../views/ProfileView.vue"),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/edit-profile",
     name: "editprofile",
     component: () => import("../views/EditProfileView.vue"),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/my-list",
     name: "mylist",
     component: () => import("../views/MyFilmView.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/add-film",
+    name: "addfilm",
+    component: () => import("../views/AddFilmView.vue"),
+    meta: {
+      requiresAdmin: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (requiresAuth && store.state.isLogin === false) next("auth");
+  else next();
 });
 
 export default router;
